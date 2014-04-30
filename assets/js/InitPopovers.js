2ebb24866742024282e4cafb12d89abcdcbe28af
matt5671110 <matthew7195@gmail.com>
2014-04-29 22:04:38 -0600

    PUSH 1.5.5.5.5.5
    Last second tweak, made popovers go away when switching tabs.

--- assets/js/InitPopovers.js
+++ assets/js/InitPopovers.js
@@ -1,3 +1,4 @@
 $(document).ready(function () {
   $(".hasPop").popover({ trigger: "click", html: true }); //Init popover for elements with class hasPop
+  $(".nav-tabs").find("li").click(function (evt) {$(".hasPop").popover('hide');});
 });
